79e6047493b24b57003973e421f3f6f5b942ff88
Nicholas DeSanctis <nd0018@uah.edu>
2021-10-10 16:33:46 -0500

    Fixed Company and Vehicle controller problems. Added OrderInfo controller

--- TCMS_Web/Controllers/VehicleController.cs
+++ TCMS_Web/Controllers/VehicleController.cs
@@ -1,24 +1,123 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using DataAccess;
+using Models;
 
 namespace TCMS_Web.Controllers
 {
     public class VehicleController : Controller
     {
-        private readonly TCMS_Context veh;
+        private readonly TCMS_Context _context;
 
-        public VehicleController (TCMS_Context vehicle)
+        public VehicleController(TCMS_Context context)
         {
-            veh = vehicle;
+            _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Vehicles
+        public async Task<IActionResult> Index()
         {
-            return View(veh.Vehicles.ToList());
+            return View(await _context.Vehicles.ToListAsync());
+        }
+
+        // GET: Vehicles/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.Vehicles
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Create
+        public IActionResult Add()
+        {
+            return View(new Vehicle());
+        }
+
+        // POST: Vehicles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Add ([Bind("Brand,Year,Model,Type,ReadyStatus,Status,Parts,LastMaintenanceDate,MaintenanceCycle")] Vehicle vehicle)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vehicle);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicle);
+        }
+
+        // GET: Vehicles/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehicle = await _context.Vehicles.FindAsync(id);
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+            return View(vehicle);
+        }
+
+        // POST: Vehicles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Brand,Year,Model,Type,ReadyStatus,Status,Parts,LastMaintenanceDate,MaintenanceCycle")] Vehicle vehicle)
+        {
+            if (id != vehicle.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vehicle);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VehicleExists(vehicle.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehicle);
+        }
+        private bool VehicleExists(string id)
+        {
+            return _context.Vehicles.Any(e => e.Id == id);
         }
     }
 }
