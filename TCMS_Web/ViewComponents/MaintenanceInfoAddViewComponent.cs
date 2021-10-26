acbd721dd485132fc49eed67f3a32f344469de4b
Nicholas DeSanctis <nd0018@uah.edu>
2021-10-26 13:17:09 -0500

    Changes made to Maintenance Info tabs

--- TCMS_Web/ViewComponents/MaintenanceInfoAddViewComponent.cs
+++ TCMS_Web/ViewComponents/MaintenanceInfoAddViewComponent.cs
@@ -8,17 +8,16 @@ using System.Threading.Tasks;
 
 namespace TCMS_Web.ViewComponents
 {
-    public class MaintenanceInfoViewComponent:ViewComponent
+    public class MaintenanceInfoAddViewComponent : ViewComponent
     {
         private readonly TCMS_Context _context;
-        public MaintenanceInfoViewComponent(TCMS_Context context)
+        public MaintenanceInfoAddViewComponent(TCMS_Context context)
         {
             _context = context;
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View(await _context.MaintenanceInfos.ToListAsync());
+            return View(await _context.MaintenanceInfos.FirstOrDefaultAsync());
         }
     }
 }
-
--- TCMS_Web/ViewComponents/MaintenanceInfoEditViewComponent.cs
+++ TCMS_Web/ViewComponents/MaintenanceInfoEditViewComponent.cs
@@ -6,19 +6,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-
 namespace TCMS_Web.ViewComponents
 {
-    public class CompanyViewComponent:ViewComponent
+    public class MaintenanceInfoEditViewComponent:ViewComponent
     {
         private readonly TCMS_Context _context;
-        public CompanyViewComponent(TCMS_Context context)
+        public MaintenanceInfoEditViewComponent(TCMS_Context context)
         {
             _context = context;
         }
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(int? id)
         {
-            return View(await _context.Companies.ToListAsync());
+            return View(await _context.MaintenanceInfos.FirstOrDefaultAsync(m => m.Id == id));
         }
     }
 }
+
