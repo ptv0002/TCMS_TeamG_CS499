918eff9fcc93abd67369c4903713ee9670f8c3b6
Nicholas DeSanctis <nd0018@uah.edu>
2021-10-29 14:01:35 -0500

    Revert "Fixed Maintenance Controller merging issues"
    This reverts commit 113f306eb7dbfbf068c0416468f356eab34c6ef5.

--- TCMS_Web/Controllers/MaintenanceController.cs
+++ TCMS_Web/Controllers/MaintenanceController.cs
@@ -9,7 +9,11 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataAccess;
+using Models;
 
 namespace TCMS_Web.Controllers
 {
@@ -32,23 +36,32 @@ namespace TCMS_Web.Controllers
             List<MaintenanceDetail> list = new();
             if (IsIncoming_Individual)
             {
-                type = "Vehicle ID (" + id + ") ";
+                type = "Vehicle ID (" + id +") ";
 
                 // This code is NOT OPTIMIZED, update need for better performance
                 foreach (var item in model)
+        // GET: Maintenance
+        public async Task<IActionResult> Index()
+
                 {
                     if (item.MaintenanceInfo.Datetime.Value.Month == month &&
                         item.MaintenanceInfo.Datetime.Value.Year == year &&
                         item.MaintenanceInfo.Status == true && item.MaintenanceInfo.VehicleId == id)
                         list.Add(item);
-
+                        
+            var tCMS_Context = _context.MaintenanceInfos.Include(m => m.Employee).Include(m => m.Vehicle);
+            return View(await tCMS_Context.ToListAsync());
                 }
             }
             else
+
+        // GET: Maintenance/Details/5
+        public async Task<IActionResult> Details(int? id)
             {
                 type = "";
                 // This code is NOT OPTIMIZED, might update need for better performance
                 foreach (var item in model)
+            if (id == null)
                 {
                     if (item.MaintenanceInfo.Datetime.Value.Month == month &&
                         item.MaintenanceInfo.Datetime.Value.Year == year &&
@@ -60,33 +73,26 @@ namespace TCMS_Web.Controllers
             ViewBag.Individual = IsIncoming_Individual;
             return View(list);
         }
-        public async Task<IActionResult> Index()
-        {
-            var tCMS_Context = _context.MaintenanceInfos.Include(m => m.Employee).Include(m => m.Vehicle);
-            return View(await tCMS_Context.ToListAsync());
-        }
-        // GET: Maintenance/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        // GET: MaintenanceController
+        public ActionResult Index()
+
             var maintenanceInfo = await _context.MaintenanceInfos
                 .Include(m => m.Employee)
                 .Include(m => m.Vehicle)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (maintenanceInfo == null)
-            {
+        {
                 return NotFound();
-            }
+        }
 
             return View(maintenanceInfo);
         }
+
+        // GET: Maintenance/Create
         public IActionResult Add()
         {
-            ViewData["EmployeeId"] = new SelectList(_context.Employees.Where(m => m.Status == true), "Id", "Id");
-            ViewData["VehicleId"] = new SelectList(_context.Vehicles.Where(m => m.Status == true), "Id", "Id");
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id");
+            ViewData["VehicleId"] = new SelectList(_context.Vehicles, "Id", "Id");
             return View(new MaintenanceInfo());
         }
 
@@ -103,27 +109,29 @@ namespace TCMS_Web.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["EmployeeId"] = new SelectList(_context.Employees.Where(m => m.Status == true), "Id", "Id", maintenanceInfo.EmployeeId);
-            ViewData["VehicleId"] = new SelectList(_context.Vehicles.Where(m => m.Status == true), "Id", "Id", maintenanceInfo.VehicleId);
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", maintenanceInfo.EmployeeId);
+            ViewData["VehicleId"] = new SelectList(_context.Vehicles, "Id", "Id", maintenanceInfo.VehicleId);
             return View(maintenanceInfo);
         }
+
         // GET: Maintenance/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
-            }
+        }
 
             var maintenanceInfo = await _context.MaintenanceInfos.FindAsync(id);
             if (maintenanceInfo == null)
-            {
+        {
                 return NotFound();
             }
-            ViewData["EmployeeId"] = new SelectList(_context.Employees.Where(m => m.Status == true), "Id", "Id", maintenanceInfo.EmployeeId);
-            ViewData["VehicleId"] = new SelectList(_context.Vehicles.Where(m => m.Status == true), "Id", "Id", maintenanceInfo.VehicleId);
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", maintenanceInfo.EmployeeId);
+            ViewData["VehicleId"] = new SelectList(_context.Vehicles, "Id", "Id", maintenanceInfo.VehicleId);
             return View(maintenanceInfo);
         }
+
         // POST: Maintenance/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -137,42 +145,40 @@ namespace TCMS_Web.Controllers
             }
 
             if (ModelState.IsValid)
+        {
+            try
             {
-                try
-                {
                     _context.Update(maintenanceInfo);
                     await _context.SaveChangesAsync();
-                }
+            }
                 catch (DbUpdateConcurrencyException)
                 {
                     if (!MaintenanceInfoExists(maintenanceInfo.Id))
-                    {
+            {
                         return NotFound();
-                    }
+        }
                     else
-                    {
+        {
                         throw;
                     }
-                }
+        }
+
+        // POST: MaintenanceController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["EmployeeId"] = new SelectList(_context.Employees.Where(m => m.Status == true), "Id", "Id", maintenanceInfo.EmployeeId);
-            ViewData["VehicleId"] = new SelectList(_context.Vehicles.Where(m => m.Status == true), "Id", "Id", maintenanceInfo.VehicleId);
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Id", maintenanceInfo.EmployeeId);
+            ViewData["VehicleId"] = new SelectList(_context.Vehicles, "Id", "Id", maintenanceInfo.VehicleId);
             return View(maintenanceInfo);
         }
         private bool MaintenanceInfoExists(int id)
-        {
+            {
             return _context.MaintenanceInfos.Any(e => e.Id == id);
         }
-
     }
 }
-
-
-
-
-
-
-
-
-
