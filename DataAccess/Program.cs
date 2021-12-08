7804e59c18ff5e77aed26d5b70ae3340256e3ff3
Nicholas DeSanctis <nd0018@uah.edu>
2021-12-07 23:36:48 -0600

    Fix issue

---
+++ DataAccess/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String ConnectionString = "Data Source=.; Initial Catalog=TCMS_DB;Integrated Security=True;MultipleActiveResultSets=True";
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Select Address FROM dbo.Company";
+            cmd.Connection = conn;
+
+            conn.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            while (sdr.Read())
+            {
+                Console.WriteLine(sdr[0]);
+            }
+
+        }
+    }
+}
